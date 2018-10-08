00c3731a7207417fe350ed35324ef9197e53f295
Hillel7 <qqqsjjj13@gmail.com>
2018-10-08 20:42:06 +0300

    Initial commit

---
+++ src/main/java/com/hillel/elementary/newproject/Main.java
@@ -0,0 +1,10 @@
+package com.hillel.elementary.newproject;
+
+/**
+ * Created by Hillel7 on 10/8/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello");
+    }
+}
